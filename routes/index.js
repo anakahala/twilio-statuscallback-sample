fb22cbff4d36b6b349a7e30bb50007e90479df1c
Akihiro Nakahara <anakahara@uewave.com>
2017-10-11 12:38:22 +0900

    updated.

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 require('dotenv').load();
-var client = require('twilio')(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+var twilioSid = process.env.TWILIO_SID;
+var twilioToken = process.env.TWILIO_TOKEN;
+var twilioTellTo = process.env.TWILIO_TEL_TO;
+var twilioTellFrom = process.env.TWILIO_TEL_FROM;
+var herokuAppName = process.env.HEROKU_APP_NAME;
+
+var client = require('twilio')(twilioSid, twilioToken);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+/* Twilio Call */
 router.post('/', function(req, res, next) {
   client.calls.create({
     url: 'https://demo.twilio.com/docs/voice.xml',
-    to: process.env.TWILIO_TEL_TO,
-    from: process.env.TWILIO_TEL_FROM,
+    to: twilioTellTo,
+    from: twilioTellFrom,
     timeout: 60,
-    statusCallback: 'https://' + process.env.HEROKU_APP_NAME + '.herokuapp.com/statusCallBack/' + req.body.socketId,
+    statusCallback: 'https://' + herokuAppName + '.herokuapp.com/statusCallBack/' + req.body.socketId,
     statusCallbackMethod: "GET",
     statusCallbackEvent: ["ringing", "answered", "completed"]
   }, function(err, call) {
